bcc7b989c8442fa1fcc901b7c2ab7e1e0ad9da02
ahmed <moode434@gmail.com>
2025-12-05 10:23:45 +0100

    Initial Commit

---
+++ sfrh/sfrhApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfrhApp.swift
+//  sfrh
+//
+//  Created by user289721 on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct sfrhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
